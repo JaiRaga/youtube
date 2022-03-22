b2887e31225325fc55d0d74678d235df9894bc2c
JaiRaga <raga.j.santhosh@gmail.com>
2022-03-22 13:54:17 +0530

    :art: added styles

--- screens/VideoScreen/styles.ts
+++ screens/VideoScreen/styles.ts
@@ -30,6 +30,49 @@ const styles = StyleSheet.create({
 		fontSize: 14,
 		fontWeight: '500',
 	},
+	actionListContainer: {
+		// flexDirection: 'row',
+		// backgroundColor: 'red' 
+	},
+	actionListItem: {
+		width: 70,
+		height: 60,
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		// backgroundColor: 'red'
+	},
+	actionText: {
+		color: 'white'
+	},
+	//User
+	avatar: {
+		width: 40,
+		height: 40,
+		borderRadius: 20
+	},
+	userContainer: {
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	userTextContainer: {
+		marginHorizontal: 10,
+		flex: 1
+	},
+	username: {
+		color: 'white',
+		fontSize: 18,
+		fontWeight: "bold"
+	},
+	subscribers: {
+		color: 'grey',
+		fontSize: 18
+	},
+	subscribe: {
+		color: 'red',
+		fontSize: 20,
+		fontWeight: 'bold',
+		padding: 10
+	}
 })
 
 export default styles
